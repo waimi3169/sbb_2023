db2d8c80e9419f3debb8b42e90b6f011bdf509bf
깃waimi3169 <waimi3169@naver.com>
2023-03-28 15:43:16 +0900

    3-09까지 구현

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -2,8 +2,11 @@ package com.mysite.sbb.answer;
 
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,22 +14,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/answer")
 @RequiredArgsConstructor
 public class AnswerController {
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String createAnswer(
             Model model,
             @PathVariable Integer id,
             @Valid AnswerForm answerForm,
-            BindingResult bindingResult
+            BindingResult bindingResult,
+            Principal principal
     ) {
         // 관련 질문을 얻어온다.
         Question question = questionService.getQuestion(id);
+        SiteUser siteUser = userService.getUser(principal.getName());
 
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
@@ -34,7 +43,7 @@ public class AnswerController {
         }
 
         // TODO : 답변객체를 만든다.
-        Answer answer = answerService.create(question, answerForm.getContent());
+        Answer answer = answerService.create(question, answerForm.getContent(), siteUser);
 
         // 아래와 같이 처리요청에 대해서는, 처리 후 빨리 떠나는 게 좋다.
         // 단 적절한 if 문을 사용하면 유용한 방법이 될 수 도 있다. 즉 머무는게 좋은 경우도 있음
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,13 +1,18 @@
 package com.mysite.sbb.question;
 
 import com.mysite.sbb.answer.AnswerForm;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/question")
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 // @Validated 컨트롤러에서는 이 부분 생략가능
 public class QuestionController {
     private final QuestionService questionService;
+    private final UserService userService;
 
     @GetMapping("/list")
     public String list(Model model, @RequestParam(defaultValue = "0") int page) {
@@ -34,6 +40,7 @@ public class QuestionController {
         return "question_detail";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/create")
     // questionForm 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
     // QuestionForm questionForm 써주는 이유, question_form.html 에서 questionForm 변수가 없으면 실행이 안되기 때문에
@@ -43,18 +50,21 @@ public class QuestionController {
         return "question_form";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create")
     // @Valid QuestionForm questionForm
     // questionForm 값을 바인딩 할 때 유효성 체크를 해라!
     // questionForm 변수와 bindingResult 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
-    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
-        if ( bindingResult.hasErrors() ) {
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
             // question_form.html 실행
             // 다시 작성하라는 의미로 응답에 폼을 실어서 보냄
             return "question_form";
         }
 
-        questionService.create(questionForm.getSubject(), questionForm.getContent());
+        SiteUser siteUser = userService.getUser(principal.getName());
+
+        questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
 
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
